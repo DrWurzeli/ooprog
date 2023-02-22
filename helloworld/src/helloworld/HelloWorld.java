4a471d5328d81b9f3e65a857bca066d39fb8e59b
BuildTools <github@maxmailbox.de>
2023-02-22 17:11:04 +0100

    test v2

--- helloworld/src/helloworld/HelloWorld.java
+++ helloworld/src/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ package helloworld;
 public class HelloWorld {
 
 	/**
-	 * @param no args for now - VSCode test
+	 * @param no args for now - Eclipse pull and push test nr2
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
