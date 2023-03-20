1e1815cdc2f19559e21f4bdfda88dc95da8aafa2
Maximilian Sander <github@maxmailbox.de>
2023-03-20 14:41:34 +0100

    initial version containing Scanner

---
+++ Teil3/src/aufgabe4/EingabeTest.java
@@ -0,0 +1,24 @@
+package aufgabe4;
+import java.util.Scanner;
+
+public class EingabeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner eingabe = new Scanner(System.in);
+		System.out.println("Erste Zahl eingeben: ");
+		
+		int zahl1 = eingabe.nextInt();
+		
+		System.out.println("Zweite Zahl eingeben: ");
+		int zahl2 = eingabe.nextInt();
+		
+		int summe = zahl1 + zahl2;
+		
+		System.out.println("Die Summe von " + zahl1 + " und " + zahl2 + " ist: " +summe);
+		
+
+	}
+
+}
