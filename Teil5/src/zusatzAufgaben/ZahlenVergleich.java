e12cbf476294dbdf59c9bca4b923722554ff6a5a
Maximilian Sander <github@maxmailbox.de>
2023-04-03 14:56:29 +0200

    completed template with simple comparison

---
+++ Teil5/src/zusatzAufgaben/ZahlenVergleich.java
@@ -0,0 +1,28 @@
+package zusatzAufgaben;
+
+import java.util.Scanner;
+
+public class ZahlenVergleich
+{
+   public static void main(String[] args)
+   {
+      Scanner scan = new Scanner(System.in);
+      System.out.print ("Gib die erste Zahl ein: ");
+      int zahl1 = scan.nextInt();
+      System.out.print ("Gib die zweite Zahl ein: ");
+      int zahl2 = scan.nextInt();
+
+      if (zahl1 > zahl2)
+      {
+         System.out.println("Zahl 1 ist größer als Zahl 2");
+      } 
+      else if (zahl2 > zahl1)
+      {
+         System.out.println("Zahl 2 ist größer als Zahl 1");
+      }
+      else
+      {
+         System.out.println("Die Zahlen sind gleich groß.");
+      }
+   }
+}
