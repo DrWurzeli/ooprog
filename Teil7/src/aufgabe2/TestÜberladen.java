f0096f104ecd50900ebc93e5c025d0697887dc70
Maximilian Sander <github@maxmailbox.de>
2023-04-04 09:30:55 +0200

    initial version

---
+++ Teil7/src/aufgabe2/TestÜberladen.java
@@ -0,0 +1,21 @@
+package aufgabe2;
+
+public class TestÜberladen {
+
+	public static void main(String[] args) {
+		
+		//void println ()
+		System.out.println();
+		
+		//void println (boolean x)
+		System.out.println(false);
+		
+		//void println (int x)
+		System.out.println(0);
+		
+		//void println (String x)
+		System.out.println("Test");
+
+	}
+
+}
