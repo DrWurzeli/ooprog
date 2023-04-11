e469e40072d25e3e7b6903b2edec0c3621bc7914
Maximilian Sander <github@maxmailbox.de>
2023-04-11 10:37:27 +0200

    initial version using class variables using static and incrementing on object creation

---
+++ Teil8/src/aufgabe5/TestBuch.java
@@ -0,0 +1,19 @@
+package aufgabe5;
+
+public class TestBuch {
+
+	public static void main(String[] args) {
+
+		Buch meinBuch = new Buch("Test", "123-4567-8910", 1);
+		
+		meinBuch.ausgeben();
+		
+		meinBuch = new Buch("Test2", "Untertitel1", "123-456-789", 2);
+		
+		meinBuch.ausgeben();
+		
+		System.out.println("Anzahl verschiedener Bücher: " + Buch.getBestand());
+		
+	}
+
+}
