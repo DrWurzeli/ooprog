7a40197e0b0a5a51232c05231ac3b751f5f609a8
Maximilian Sander <github@maxmailbox.de>
2023-04-11 11:15:27 +0200

    array for easier access and foreach for output

--- Teil8/src/aufgabe6/TestKinosaal.java
+++ Teil8/src/aufgabe6/TestKinosaal.java
@@ -4,11 +4,18 @@ public class TestKinosaal {
 
 	public static void main(String[] args) {
 		// 2 Kinosäle mit 50 bzw. 100 Plätzen
-		Kinosaal test = new Kinosaal();
-		test.setAnzahlSitzplaetzeSaal(10);
+		Kinosaal[] saele = new Kinosaal[2];
+		saele[0] = new Kinosaal();
+		saele[1] = new Kinosaal();
+		saele[0].setAnzahlSitzplaetzeSaal(50);
+		saele[1].setAnzahlSitzplaetzeSaal(100);
 		
-		System.out.println("Saal: " + test.getAnzahlSitzplaetzeSaal() + " " + test.getSaalNummer());
-		System.out.println("Kino: " + Kinosaal.getAnzahlSitzplaetzeKino() + " " + Kinosaal.getAnzahlKinosaele());
+		for(Kinosaal x : saele) {
+			System.out.println("Saal: " + x.getSaalNummer() + " Plaetze: " + x.getAnzahlSitzplaetzeSaal());
+		}
+		
+		System.out.println("Kinoplaetze: " + Kinosaal.getAnzahlSitzplaetzeKino() + " Saele: " + Kinosaal.getAnzahlKinosaele());
+	
 	}
 
 }
