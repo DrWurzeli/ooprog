9a17e2894118c0b86ddc84faac6a336591280a88
Maximilian Sander <github@maxmailbox.de>
2023-04-11 12:30:57 +0200

    updated Stack.java

--- Teil8/src/aufgabe7/Stack.java
+++ Teil8/src/aufgabe7/Stack.java
@@ -3,26 +3,22 @@ package aufgabe7;
 public class Stack {
 	
 	private int[] arrayStack = null;
+	private int current = -1;
 	
 	public Stack(int size) {
 		arrayStack = new int [size]; 
 	}
 	
 	public void push(int u) {
-		
+		arrayStack[++current] = u;
 	}
 	
 	public int pop() {
-		return 0;
+		return arrayStack[current--];
 	}
 	
 	public boolean isEmpty() {
-		if(arrayStack == null) {
-			return false;
-		}
-		else {
-			return true;
-		}
+		return current == -1;
 	}
 	
 	public void stackPrint() {
