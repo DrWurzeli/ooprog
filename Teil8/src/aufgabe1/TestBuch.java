f5028b8e397da5f5ae73e513492128c6b4a8a501
Maximilian Sander <github@maxmailbox.de>
2023-04-11 09:05:29 +0200

    small layout change

--- Teil8/src/aufgabe1/TestBuch.java
+++ Teil8/src/aufgabe1/TestBuch.java
@@ -3,14 +3,16 @@ package aufgabe1;
 public class TestBuch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Buch meinBuch = new Buch();
 		
 		meinBuch.setTitel("Hello world!");
 		meinBuch.setIsbn("1234567890");
 		meinBuch.setAnzahlExemplare(1);
 		
-		System.out.println("Mein Buch hat den Titel: " + meinBuch.getTitel() + "\nund die ISBN: " + meinBuch.getIsbn() + "\nes gibt " + meinBuch.getAnzahlExemplare()+ " Examplare.");
+		System.out.println("Mein Buch hat den Titel: " + meinBuch.getTitel()
+			+ "\nund die ISBN: " + meinBuch.getIsbn()
+			+ "\nes gibt " + meinBuch.getAnzahlExemplare() + " Examplare.");
 		
 	}
 
