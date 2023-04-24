386b8574da0cfd1057dc27ab42972a5ea471abab
Maximilian Sander <github@maxmailbox.de>
2023-04-24 14:24:21 +0200

    constructor for Zeitschrift

--- Teil9/src/aufgabe5/Zeitschrift.java
+++ Teil9/src/aufgabe5/Zeitschrift.java
@@ -8,6 +8,18 @@ public class Zeitschrift {
 	
 	static int bestand;
 	
+	public Zeitschrift(String titel, String issn, int anzahlExemplare) {
+		this(titel, "", issn, anzahlExemplare);
+	}
+	
+	public Zeitschrift(String titel, String unterTitel, String issn, int anzahlExemplare) {
+		this.titel = titel;
+		this.unterTitel = unterTitel;
+		this.issn = issn;
+		this.anzahlExemplare = anzahlExemplare;
+		bestand++;
+	}
+	
 	public void ausgeben() {
 		System.out.println("Titel: " + titel + "\nUntertitel: " + unterTitel + "\nISSN: " + issn);
 	}
