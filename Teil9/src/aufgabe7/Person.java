81d62112d5d64a1148e85d0fe33f9ef7074f458a
Maximilian Sander <github@maxmailbox.de>
2023-05-02 08:57:19 +0200

    override toString for class identification

---
+++ Teil9/src/aufgabe7/Person.java
@@ -0,0 +1,20 @@
+package aufgabe7;
+
+public class Person {
+	private String vorname;
+	private String name;
+	
+	public Person(String vorname, String name) {
+		this.vorname = vorname;
+		this.name = name;
+	}
+	
+	public void ausgeben() {
+		System.out.println("Vorname: " + vorname + "\nNachname: " + name);
+	}
+	
+	@Override
+	public String toString() {
+		return vorname + " " + name;
+	}
+}
---
+++ Teil9/src/aufgabe7/TestAusleiher.java
@@ -0,0 +1,11 @@
+package aufgabe7;
+
+public class TestAusleiher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ausleiher a1 = new Ausleiher("Max", "Mustermann", 100);
+		
+		System.out.println(a1);	}
+
+}
