b889956c7a3e9129fcd7e0a0c3b226f6c889ce11
Maximilian Sander <github@maxmailbox.de>
2023-05-02 09:07:11 +0200

    updated classes from aufgabe4 to use toString instead of custom ausgeben

---
+++ Teil9/src/aufgabe8/Buch.java
@@ -0,0 +1,20 @@
+package aufgabe8;
+
+public class Buch extends Medium{
+	
+	private static int buchBestand;
+	
+	public Buch(String titel, String untertitel, String mediumNummer, int anzahlExemplare) {
+		super(titel, untertitel, mediumNummer, anzahlExemplare);
+		buchBestand++;
+	}
+	
+	public Buch(String titel, String mediumNummer, int anzahlExemplare) {
+		super(titel, mediumNummer, anzahlExemplare);
+	}
+	
+	public static int getBuchBestand() {
+		return buchBestand;
+	}
+	
+}
