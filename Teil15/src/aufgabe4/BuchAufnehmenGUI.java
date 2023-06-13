eb8442ee4e51b3b128c561a8761519df84458dbc
BuildTools <github@maxmailbox.de>
2023-06-13 19:59:41 +0200

    small layout adjustment

--- Teil15/src/aufgabe4/BuchAufnehmenGUI.java
+++ Teil15/src/aufgabe4/BuchAufnehmenGUI.java
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -16,10 +18,10 @@ import aufgabe4.entitaeten.Buch;
 public class BuchAufnehmenGUI extends JDialog {
    private JButton aufnehmen = new JButton("Buch aufnehmen");
    private JButton abbrechen = new JButton("Abbrechen");
-   private JTextField titel = new JTextField (30);
-   private JTextField unterTitel = new JTextField (30);
-   private JTextField isbn = new JTextField (30);
-   private JTextField anzahl = new JTextField (10);
+   private JTextField titel = new JTextField(50);
+   private JTextField unterTitel = new JTextField(50);
+   private JTextField isbn = new JTextField(50);
+   private JTextField anzahl = new JTextField(50);
 
    private static final long serialVersionUID = 1L;
 
@@ -34,38 +36,42 @@ public class BuchAufnehmenGUI extends JDialog {
 
       dialogAufbauen();
       controllerHinzufuegen();
-
+      
       pack();
+      setMinimumSize(new Dimension(300,300));
       setVisible (true);
    }
 
    private void dialogAufbauen() {
+	  FlowLayout left = new FlowLayout(FlowLayout.LEFT);
+	  FlowLayout center = new FlowLayout(FlowLayout.CENTER);
+	   
       JPanel pane1 = new JPanel();
-      pane1.setLayout (new FlowLayout(FlowLayout.LEFT));
-      pane1.add (new JLabel ("Titel:         "));
+      pane1.setLayout(left);
+      pane1.add (new JLabel ("Titel: "));
       pane1.add (titel);
       add (pane1);
 
       JPanel pane2 = new JPanel();
-      pane2.setLayout(new FlowLayout(FlowLayout.LEFT));
-      pane2.add (new JLabel ("Untertitel:         "));
+      pane2.setLayout(left);
+      pane2.add (new JLabel ("Untertitel: "));
       pane2.add (unterTitel);
       add (pane2);
 
       JPanel pane3 = new JPanel();
-      pane3.setLayout(new FlowLayout(FlowLayout.LEFT));
-      pane3.add (new JLabel ("ISBN:         "));
+      pane3.setLayout(left);
+      pane3.add (new JLabel ("ISBN: "));
       pane3.add (isbn);
       add (pane3);
 
       JPanel pane4 = new JPanel();
-      pane4.setLayout(new FlowLayout(FlowLayout.LEFT));
-      pane4.add (new JLabel ("Anzahl:         "));
+      pane4.setLayout(left);
+      pane4.add (new JLabel ("Anzahl: "));
       pane4.add (anzahl);
       add (pane4);
 
       JPanel pane5 = new JPanel();
-      pane5.setLayout (new FlowLayout(FlowLayout.LEFT));
+      pane5.setLayout (center);
       pane5.add (aufnehmen);
       pane5.add (abbrechen);
       add (pane5);
